3acd8fba7f3d76e8c6fdf315dc9a4ffde8bedc12
Sayemsaadat0 <sayemsaadat0@gmail.com>
2025-01-07 10:07:36 +0600

    feat: layout

--- src/app/(seller)/template.tsx
+++ src/app/(seller)/template.tsx
@@ -1,19 +1,17 @@
-import DefaultFooter from '@/components/core/footer/DefaultFooter'
-import DefaultNavbar from '@/components/core/navbar/DefaultNavbar'
-import { Toaster } from '@/components/ui/toaster'
-import React from 'react'
+import DefaultFooter from '@/components/core/footer/DefaultFooter';
+import DefaultNavbar from '@/components/core/navbar/DefaultNavbar';
+import { Toaster } from '@/components/ui/toaster';
+import React from 'react';
 
 const template = ({ children }: { children: React.ReactNode }) => {
-    return (
-        <div>
-            <DefaultNavbar />
-            <div className='mt-44 md:mt-50 min-h-[90vh] '>
-                {children}
-            </div>
-            <DefaultFooter />
-            <Toaster />
-        </div>
-    )
-}
+  return (
+    <div>
+      <DefaultNavbar />
+      <div className="mt-44 md:mt-50 min-h-[90vh] ">{children}</div>
+      <DefaultFooter />
+      <Toaster />
+    </div>
+  );
+};
 
-export default template
+export default template;
