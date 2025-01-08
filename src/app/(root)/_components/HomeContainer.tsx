8db6c7af2ecc20480027fda8d6afeb8930c711f4
shafik720 <shafikrasel5@gmail.com>
2025-01-08 18:03:35 +0600

    chore: added homepage

--- src/app/(root)/_components/HomeContainer.tsx
+++ src/app/(root)/_components/HomeContainer.tsx
@@ -1,49 +1,48 @@
-import React from 'react'
-import HeroSection from './HeroSection'
-import HomeCategorySection from './HomeCategorySection'
-import NewArrivalSection from './NewArrivalSection'
-import FirstAddSection from './FirstAddSection'
-import OfferSection from './OfferSection'
-import TrendingProducts from './TrendingProducts'
-import SecondAddSection from './SecondAddSection'
-import BlogSection from './BlogSection'
-import TopBrands from './TopBrands'
+import React from 'react';
+import HeroSection from './HeroSection';
+import HomeCategorySection from './HomeCategorySection';
+import NewArrivalSection from './NewArrivalSection';
+import FirstAddSection from './FirstAddSection';
+import OfferSection from './OfferSection';
+import TrendingProducts from './TrendingProducts';
+import SecondAddSection from './SecondAddSection';
+import BlogSection from './BlogSection';
+import TopBrands from './TopBrands';
 
 const HomeContainer = () => {
   return (
     <div className=" space-y-[40px] md:space-y-[80px]">
-      <div className='fly-commonContainer'>
+      <div className="fly-commonContainer">
         <HeroSection />
       </div>
-      <div className='fly-commonContainer'>
+      <div className="fly-commonContainer">
         <HomeCategorySection />
       </div>
-      <div className=''>
+      <div className="">
         <OfferSection />
       </div>
 
-      <div className='fly-commonContainer'>
+      <div className="fly-commonContainer">
         <TopBrands />
       </div>
-      <div className='fly-commonContainer'>
+      <div className="fly-commonContainer">
         <FirstAddSection />
       </div>
 
-
-      <div className='fly-commonContainer'>
+      <div className="fly-commonContainer">
         <TrendingProducts />
       </div>
-      <div className='fly-commonContainer'>
+      <div className="fly-commonContainer">
         <SecondAddSection />
       </div>
-      <div className=''>
+      <div className="">
         <NewArrivalSection />
       </div>
-      <div className='pb-10 md:pb-[80px] fly-commonContainer'>
+      <div className="pb-10 md:pb-[80px] fly-commonContainer">
         <BlogSection />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomeContainer
+export default HomeContainer;
--- src/app/(root)/_components/SecondAddSection.tsx
+++ src/app/(root)/_components/SecondAddSection.tsx
@@ -1,12 +1,12 @@
-import Button from '@/components/ui/button2'
-import { SearchIcon } from 'lucide-react'
-import Image from 'next/legacy/image'
-import React from 'react'
+import Button from '@/components/ui/button2';
+import { SearchIcon } from 'lucide-react';
+import Image from 'next/legacy/image';
+import React from 'react';
 
 const SecondAddSection = () => {
   return (
-    <div className='flex  flex-col md:flex-row items-center justify-center p-5 w-full bg-f-primary-1-50 rounded-[20px]'>
-      <div className='w-full  flex items-center justify-center'>
+    <div className="flex  flex-col md:flex-row items-center justify-center p-5 w-full bg-f-primary-1-50 rounded-[20px]">
+      <div className="w-full  flex items-center justify-center">
         <Image
           className="aspect-[1/1] object-cover w-full"
           src={'/ITEM.png'}
@@ -15,12 +15,15 @@ const SecondAddSection = () => {
           height={500}
         />
       </div>
-      <div className='w-full border space-y-10'>
-        <h3 className='text-w-header-1-bold-22  md:text-w-header-4-bold-56'>Are You Missing Anything? <span className='text-f-primary-1-600'>Explore</span> Travel Must-Haves!</h3>
-        <Button icon={<SearchIcon />} label='Explore'/>
+      <div className="w-full border space-y-10">
+        <h3 className="text-w-header-1-bold-22  md:text-w-header-4-bold-56">
+          Are You Missing Anything? <span className="text-f-primary-1-600">Explore</span> Travel
+          Must-Haves!
+        </h3>
+        <Button icon={<SearchIcon />} label="Explore" />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SecondAddSection
+export default SecondAddSection;
